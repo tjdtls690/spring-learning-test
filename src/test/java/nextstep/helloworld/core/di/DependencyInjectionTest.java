327230e8ffd19f93bbe67513ab6f23cc82cc915a
JunH <tjdtls690@gmail.com>
2023-04-18 11:34:33 +0900

    :bug: fix : 잘못 작성된 주석 고치기

--- src/test/java/nextstep/helloworld/core/di/DependencyInjectionTest.java
+++ src/test/java/nextstep/helloworld/core/di/DependencyInjectionTest.java
@@ -21,7 +21,7 @@ public class DependencyInjectionTest {
     }
 
     /**
-     * TODO: 필드 주입을 통해 StationConstructorService에 StationRepository 의존성을 주입하기
+     * TODO: 필드 주입을 통해 StationFieldService에 StationRepository 의존성을 주입하기
      */
     @Test
     void autowiredInjection() {
@@ -31,7 +31,7 @@ public class DependencyInjectionTest {
     }
 
     /**
-     * TODO: setter 주입을 통해 StationConstructorService에 StationRepository 의존성을 주입하기
+     * TODO: setter 주입을 통해 StationSetterService에 StationRepository 의존성을 주입하기
      */
     @Test
     void setterInjection() {
